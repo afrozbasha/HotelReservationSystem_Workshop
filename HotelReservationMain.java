e34061e9d5e35a698f8919fa84f6deaf71abb4d7
afrozbasha <iafrozbasha@gmail.com>
2021-08-11 14:10:33 +0530

    Ability to add Hotel in a Hotel Reservation System with Name and rates in WeekDays and WeekEnds

--- HotelReservationMain.java
+++ HotelReservationMain.java
@@ -1,15 +1,16 @@
 package com.HotelReservation;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
 
-
-
-public class HotelReservationMain {
-
+public class HotelReservationSystem {
     public static void main(String[] args) {
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.addHotel("LakeWood", 110, 90);
+        hotelReservation.addHotel("BridgeWood", 150, 50);
+        hotelReservation.addHotel("RidgeWood", 220, 150);
+        hotelReservation.getHotelDetails();
 
-            HotelReservation hotelReservation = new HotelReservation();
-            hotelReservation.viewHotels();
-            hotelReservation.getMinPriceHotel();
+     
     }
 }

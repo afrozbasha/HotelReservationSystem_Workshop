0c7a6e31cd203f6ab1ac975d7cab371bfb3e5dff
afrozbasha <iafrozbasha@gmail.com>
2021-08-11 14:00:41 +0530

    Ability to add Hotel in a Hotel Reservation System with Name and rates

---
+++ HotelReservationMain.java
@@ -0,0 +1,15 @@
+package com.HotelReservation;
+
+import java.util.*;
+
+
+
+public class HotelReservationMain {
+
+    public static void main(String[] args) {
+
+            HotelReservation hotelReservation = new HotelReservation();
+            hotelReservation.viewHotels();
+          
+    }
+}

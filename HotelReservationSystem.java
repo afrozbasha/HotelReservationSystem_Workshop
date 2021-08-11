6a4e4b754d676d7aca4fa5a505a9eb92890dbf17
afrozbasha <iafrozbasha@gmail.com>
2021-08-11 16:10:48 +0530

    Ability to add Hotel in a Hotel Reservation System with Name and rates in WeekDays and WeekEnds with best rates and shows how much coast shows rating for each hotel

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -1,31 +1,114 @@
 package com.HotelReservation;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
 
-import java.time.Month;
-import java.time.temporal.ChronoUnit;
-
-public class HotelReservationSystem {
-    public static void main(String[] args) {
-        HotelReservation hotelReservation = new HotelReservation();
-        hotelReservation.addHotel("LakeWood", 110, 90);
-        hotelReservation.addHotel("BridgeWood", 150, 50);
-        hotelReservation.addHotel("RidgeWood", 220, 150);
-        //list out hotel Details
-        hotelReservation.getHotelDetails();
-
-        //giving start and end dates of the schedule
-        LocalDate dateStart = LocalDate.of(2021, Month.AUGUST, 11);
-        LocalDate dateEnd = LocalDate.of(2021, Month.AUGUST, 20);
-        System.out.println("Starting date : "+dateStart);
-        System.out.println("Ending date : "+dateEnd);
-        int noOfDays = (int) ChronoUnit.DAYS.between(dateStart, dateEnd);
-        System.out.println("Total days is : "+noOfDays);
-        //finding getWeekDaysCount
-        int weekEndsCount = hotelReservation.getWeekDaysCount(dateStart, noOfDays);
-        int weekDaysCount = noOfDays - weekEndsCount;
-
-        //find getMinPriceHotel
-        hotelReservation.getMinPriceHotel(weekEndsCount, weekDaysCount);
-        
+class Hotel{
+    public String getHotelName() {
+        return hotelName;
     }
+    public Hotel(){};
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public int getHotelPriceWeekDays() {
+        return hotelPriceWeekDays;
+    }
+
+    public void setHotelPriceWeekDays(int hotelPriceWeekDays) {
+        this.hotelPriceWeekDays = hotelPriceWeekDays;
+    }
+
+    public int getHotelPriceWeekEnds() {
+        return hotelPriceWeekEnds;
+    }
+
+    public void setHotelPriceWeekEnds(int hotelPriceWeekEnds) {
+        this.hotelPriceWeekEnds = hotelPriceWeekEnds;
+    }
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "hotelName='" + hotelName + '\'' +
+                ",hotelPriceWeekDays=" + hotelPriceWeekDays +
+                ",hotelPriceWeekEnds=" + hotelPriceWeekEnds +
+                ", hotelRating= "+ hotelRating+
+                '}'+"\n";
+    }
+
+    public Hotel(String hotelName, int hotelPriceWeekDays, int hotelPriceWeekEnds, int hotelRating) {
+        this.hotelName = hotelName;
+        this.hotelPriceWeekDays = hotelPriceWeekDays;
+        this.hotelPriceWeekEnds = hotelPriceWeekEnds;
+        this.hotelRating = hotelRating;
+    }
+
+    private String hotelName;
+    private int hotelPriceWeekDays;
+    private int hotelPriceWeekEnds;
+    private int hotelRating;
+
+    public int getHotelRating() {
+        return hotelRating;
+    }
+
+    public void setHotelRating(int hotelRating) {
+        this.hotelRating = hotelRating;
+    }
+
+
+
+}
+
+//this class will do booking hotels and list of hotels and finding weekdays count and finding best rate hotel
+class HotelReservation extends Hotel {
+    public ArrayList<Hotel> hotelDetails = new ArrayList<Hotel>();
+
+    //ooking hotels and list of hotels
+    public void addHotel(String hotelName, int hotelPriceWeekDays, int hotelPriceWeekEnds, int hotelRating) {
+        Hotel adder = new Hotel(hotelName, hotelPriceWeekDays, hotelPriceWeekEnds, hotelRating);
+        hotelDetails.add(adder);
+    }
+    public void getHotelDetails(){
+        System.out.println(hotelDetails);
+    }
+   // finding weekEnds count
+    public int getWeekDaysCount(LocalDate dateStart, int noOfDays){
+        int weekEnds = 0;
+        int temp = noOfDays;
+        while (noOfDays>0){
+            String d = dateStart.getDayOfWeek().toString();
+            if ( (d.equals("SUNDAY")) || (d.equals("SATURDAY")) ) {
+                weekEnds++;
+            }
+            dateStart = dateStart.plusDays(1);
+            noOfDays--;
+        }
+        System.out.println("WeekEnd days count is :"+weekEnds);
+        System.out.println("WeekDay days count is :"+(temp-weekEnds));
+        return weekEnds;
+    }
+    //finding best rate hotel
+    public void getMinPriceHotel(int weekEndsCount, int weekDaysCount){
+        Hotel minWeekDays =  hotelDetails.stream().min(Comparator.comparing(Hotel::getHotelPriceWeekDays)).orElseThrow();
+        Hotel minWeekEnd =  hotelDetails.stream().min(Comparator.comparing(Hotel::getHotelPriceWeekEnds)).orElseThrow();
+        long totalRate1=0;
+        long totalRate2=0;
+        totalRate1 =  (weekEndsCount) * (minWeekDays.getHotelPriceWeekEnds()) + (weekDaysCount) * (minWeekDays.getHotelPriceWeekDays());
+        totalRate2 = (weekEndsCount) * (minWeekEnd.getHotelPriceWeekEnds()) + (weekDaysCount) * (minWeekEnd.getHotelPriceWeekDays());
+
+        if (totalRate1<totalRate2){
+            System.out.println("Best rate Hotel is : "+minWeekDays.getHotelName());
+            System.out.println(minWeekDays);
+            System.out.println(minWeekDays.getHotelName()+" Total Rate is : "+totalRate1);
+        } else{
+            System.out.println("Best Price rate Hotel is : "+minWeekEnd.getHotelName());
+            System.out.println(minWeekEnd);
+            System.out.println(minWeekEnd.getHotelName()+" Total Rate is : "+totalRate2);
+        }
+    }
+
 }
